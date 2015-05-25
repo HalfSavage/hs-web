c2fc70c23c5d3fb5bd51dad7e2f5954fe0b0935e
booty <johnedmundrose@gmail.com>
2015-05-25 17:15:18 -0400

    Install devise

--- db/migrate/20150525210525_create_users.rb
+++ db/migrate/20150525210525_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string :type
       t.string :login
       t.references :gender, index: true, foreign_key: true
-      t.string :email
       t.boolean :active
 
       t.timestamps null: false
